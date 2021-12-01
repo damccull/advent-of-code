022f5a37cdf20baa0dca261bf5272fddff556ccb
damccull <401104+damccull@users.noreply.github.com>
2021-12-01 13:35:52 -0500

    Solve second puzzle.

--- day1/puzzle1_sonar_sweep/src/lib.rs
+++ day1/puzzle1_sonar_sweep/src/lib.rs
@@ -6,14 +6,29 @@ use std::{
 
 pub fn count_depths(filename: &str) -> i32 {
     let depths = extract_depths(filename);
-    depths
-        .iter()
-        .zip(depths.iter().skip(1))
-        .fold(0, |sum, (current, next)| {
+    depths.iter().zip(depths.iter().skip(1)).fold(
+        0_i32,
+        |sum, (current, next)| {
             if current < next {
-                return sum + 1;
-            };
-            sum
+                sum + 1
+            } else {
+                sum
+            }
+        },
+    )
+}
+
+pub fn count_depths_windowed(filename: &str, window_size: usize) -> i32 {
+    let depths = extract_depths(filename);
+    depths
+        .windows(window_size)
+        .zip(depths.windows(window_size).skip(1))
+        .fold(0_i32, |sum, (current, next)| {
+            if current.iter().sum::<i32>() < next.iter().sum() {
+                sum + 1
+            } else {
+                sum
+            }
         })
 }
 
--- day1/puzzle1_sonar_sweep/src/main.rs
+++ day1/puzzle1_sonar_sweep/src/main.rs
@@ -1,5 +1,9 @@
-use puzzle1_sonar_sweep::count_depths;
+use puzzle1_sonar_sweep::{count_depths, count_depths_windowed};
 
 fn main() {
     println!("Depth Increases: {}", count_depths("input.txt"));
+    println!(
+        "Depth Increases (3-measure window): {}",
+        count_depths_windowed("input.txt", 3)
+    );
 }
