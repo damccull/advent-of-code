f4939bbc0123bb19d70dab6fe7fc3dd7fcfbd765
damccull <401104+damccull@users.noreply.github.com>
2021-12-01 13:52:46 -0500

    Additional labeling for puzzle answers.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use advent_of_code_2021::day1::{puzzle1::count_depths, puzzle2::count_depths_windowed};
 
 fn main() {
-    println!("Depth Increases: {}", count_depths("input.txt"));
+    println!("D1P1: Depth Increases: {}", count_depths("input.txt"));
     println!(
-        "Depth Increases (3-measure window): {}",
+        "D1P2: Depth Increases (3-measure window): {}",
         count_depths_windowed("input.txt", 3)
     );
 }
