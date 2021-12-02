f3288ac4b3bfa74dc30266125ecac5007bfce5f9
damccull <401104+damccull@users.noreply.github.com>
2021-12-02 09:23:47 -0500

    Move and rename input files.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use advent_of_code_2021::day1::{puzzle1::count_depths, puzzle2::count_depths_windowed};
 
 fn main() {
-    println!("D1P1: Depth Increases: {}", count_depths("input.txt"));
+    println!("D1P1: Depth Increases: {}", count_depths("input/day1.txt"));
     println!(
         "D1P2: Depth Increases (3-measure window): {}",
-        count_depths_windowed("input.txt", 3)
+        count_depths_windowed("input/day1.txt", 3)
     );
 }
