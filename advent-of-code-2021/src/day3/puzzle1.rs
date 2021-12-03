1fdf6b937f8ab4c8a675b1b66d747be0f29c58e4
damccull <401104+damccull@users.noreply.github.com>
2021-12-03 01:55:54 -0500

    Remove debug command

--- advent-of-code-2021/src/day3/puzzle1.rs
+++ advent-of-code-2021/src/day3/puzzle1.rs
@@ -4,13 +4,5 @@ use super::get_diagnostic_report;
 
 pub fn get_diagnostics(filename: PathBuf) -> u32 {
     let report = get_diagnostic_report(filename);
-    dbg!(
-        report.get_gamma_rate_binary(),
-        report.get_epsilon_rate_binary(),
-        report.get_gamma_rate_decimal(),
-        report.get_epsilon_rate_decimal(),
-        true as u8,
-        false as u8,
-    );
     report.get_gamma_rate_decimal() as u32 * report.get_epsilon_rate_decimal() as u32
 }
