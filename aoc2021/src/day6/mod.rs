411f66b3525a7fe1fac27fa07381fdec49200e89
damccull <401104+damccull@users.noreply.github.com>
2021-12-06 11:39:07 -0500

    2021 Day 6 Puzzle 1 solved

--- aoc2021/src/day6/mod.rs
+++ aoc2021/src/day6/mod.rs
@@ -1,4 +1,33 @@
+use std::path::PathBuf;
+
+use crate::{data_file, day6::puzzle1::count_fish_in_pond, read_lines};
+
 pub mod puzzle1;
 pub mod puzzle2;
 
-pub fn run() {}
+pub fn run() {
+    let pond = get_data(data_file("day6.txt"));
+
+    println!("D6P1: Fish in the pond: {}", count_fish_in_pond(pond));
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Fish {
+    age: u32,
+}
+
+pub fn get_data(filename: PathBuf) -> Vec<Fish> {
+    let mut x = Vec::new();
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines.flatten() {
+            let mut fishies = line
+                .split(',')
+                .map(|f| Fish {
+                    age: f.parse().unwrap(),
+                })
+                .collect::<Vec<_>>();
+            x.append(&mut fishies);
+        }
+    }
+    x
+}
--- aoc2021/src/day6/puzzle1.rs
+++ aoc2021/src/day6/puzzle1.rs
@@ -1,1 +1,17 @@
+use super::Fish;
 
+pub fn count_fish_in_pond(pond: Vec<Fish>) -> usize {
+    let mut pond = pond;
+    //dbg!(&pond);
+    for _day in 0..80 {
+        let fishies = &pond.clone();
+        for (i, fish) in fishies.iter().enumerate() {
+            if fish.age == 0 {
+                pond[i].age = 7;
+                pond.push(Fish { age: 8 });
+            }
+            pond[i].age -= 1;
+        }
+    }
+    pond.len()
+}
