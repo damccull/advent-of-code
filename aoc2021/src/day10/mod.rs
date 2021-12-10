14582eedcc87214db922b8e39da0a6e0602795bd
damccull <401104+damccull@users.noreply.github.com>
2021-12-10 08:52:51 -0500

    Initial thoughts for day10

---
+++ aoc2021/src/day10/mod.rs
@@ -0,0 +1,107 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+// Keep these counters:
+// Total openers
+// Total closers
+// Openers (for each type)
+// Closers (for each type)
+// If the totals don't match, line is probably incomplete
+// If totals do match, line is corrupt
+
+pub fn run() {}
+
+fn parse_line(string: &str) -> LineResult {
+    let characters = UnicodeSegmentation::graphemes(string, true).collect::<Vec<_>>();
+
+    let mut total_open = 0;
+    let mut total_close = 0;
+
+    let mut paren_left = 0;
+    let mut paren_right = 0;
+
+    let mut curly_left = 0;
+    let mut curly_right = 0;
+
+    let mut square_left = 0;
+    let mut square_right = 0;
+
+    let mut angle_left = 0;
+    let mut angle_right = 0;
+
+    for c in characters {
+        match c {
+            "(" => {
+                total_open += 1;
+                paren_left += 1;
+            }
+            ")" => {
+                total_close += 1;
+                paren_right += 1;
+            }
+            "{" => {
+                total_open += 1;
+                curly_left += 1;
+            }
+            "}" => {
+                total_close += 1;
+                curly_right += 1;
+            }
+            "[" => {
+                total_open += 1;
+                square_left += 1;
+            }
+            "]" => {
+                total_close += 1;
+                square_right += 1;
+            }
+            "<" => {
+                total_open += 1;
+                angle_left += 1;
+            }
+            ">" => {
+                total_close += 1;
+                angle_right += 1;
+            }
+            _ => {}
+        }
+
+        // If any closer is higher than opener, mark line corrupt
+        if total_close > total_open
+            || paren_right > paren_left
+            || curly_right > curly_left
+            || square_right > square_left
+            || angle_right > angle_left
+        {
+            return LineResult::Corrupt;
+        }
+
+        if total_close == total_open
+            || paren_right != paren_left
+            || curly_right != curly_left
+            || square_right != square_left
+            || angle_right != angle_left
+            || paren_right > paren_left
+            || curly_right > curly_left
+            || square_right > square_left
+            || angle_right > angle_left
+        {
+            return LineResult::Corrupt;
+        }
+
+        if total_close != total_open
+            || paren_right != paren_left
+            || curly_right != curly_left
+            || square_right != square_left
+            || angle_right != angle_left
+        {
+            return LineResult::Incomplete;
+        }
+    }
+    LineResult::Good
+}
+
+enum LineResult {
+    Good,
+    Corrupt,
+    Incomplete,
+}
--- aoc2021/src/lib.rs
+++ aoc2021/src/lib.rs
@@ -8,6 +8,7 @@ pub mod day5;
 pub mod day6;
 pub mod day7;
 pub mod day9;
+pub mod day10;
 
 pub fn data_file(filename: &str) -> PathBuf {
     aoclib::data_file(env!("CARGO_MANIFEST_DIR"), filename)
