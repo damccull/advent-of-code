4ddb0228d561ff4dfa1da22fc585522018462b91
damccull <401104+damccull@users.noreply.github.com>
2022-12-02 14:55:52 -0500

    Finish creating workspace

--- 2015/src/bin/day1.rs
+++ 2015/src/bin/day1.rs
@@ -1,4 +1,4 @@
-use advent_of_code::read_data_from_file;
+use advent_of_code_common::read_data_from_file;
 
 fn main() -> Result<(), anyhow::Error> {
     let data = read_data_from_file("data/2015-day1.txt".to_string())?;
