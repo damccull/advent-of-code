882a96ac68d83c3f3205eb836aa49c783ae976d0
damccull <401104+damccull@users.noreply.github.com>
2022-12-09 09:11:44 -0500

    Fix silly code

--- 2022/src/day4.rs
+++ 2022/src/day4.rs
@@ -82,27 +82,17 @@ impl Assignment {
         Self { beginning, end }
     }
     pub fn overlaps_with(&self, other: &Assignment) -> bool {
-        if self.beginning >= other.beginning && self.beginning <= other.end
+        self.beginning >= other.beginning && self.beginning <= other.end
             || self.end >= other.beginning && self.end <= other.end
             || other.beginning >= self.beginning && other.beginning <= self.end
             || other.end >= self.beginning && other.end <= self.end
-        {
-            return true;
-        }
-
-        false
     }
 
     pub fn fully_contains(&self, other: &Assignment) -> bool {
-        if self.beginning >= other.beginning
+        self.beginning >= other.beginning
             && self.beginning <= other.end
             && self.end >= other.beginning
             && self.end <= other.end
-        {
-            return true;
-        }
-
-        false
     }
 }
 
