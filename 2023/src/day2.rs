32552ce0fe8261f77ebec53b72dc6357f475fca6
damccull <401104+damccull@users.noreply.github.com>
2023-12-06 01:06:52 -1000

    Begin day 2 parsing

--- 2023/src/day2.rs
+++ 2023/src/day2.rs
@@ -1,11 +1,35 @@
 #[allow(unused_imports)]
 use advent_of_code_common::read_data_from_file;
+use nom::{bytes::complete::tag, character::complete::{u32, line_ending}, Parser, sequence::separated_pair};
 
 fn main() -> Result<(), anyhow::Error> {
+    let data = include_str!("../data/day2puzzle1-test.txt");
+    dbg!(parse_game().parse(data)?);
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    
+fn parse_game<'i: 't, 't>() -> impl Parser<&'i str, u32, ()> + 't {
+    move |input| {
+        let (tail, _) = tag("Game ").parse(input)?;
+        let (tail, value2) = u32(tail)?;
+        let (tail,_) = tag(": ").parse(tail)?;
+        Ok((tail, value2))
+    }
 }
+
+// type NomResult<T, U> = IResult<T, U, VerboseError<T>>;
+
+// #[derive(Debug)]
+// struct Game {
+//     id: u32,
+//     draws: Vec<Draw>,
+// }
+
+// #[derive(Debug)]
+// struct Draw {
+//     reds: u32,
+//     greens: u32,
+//     blues: u32,
+// }
+#[cfg(test)]
+mod tests {}
