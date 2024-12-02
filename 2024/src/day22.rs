e1bb587178023a4e61676c4446ddeb5ac897a085
damccull <401104+damccull@users.noreply.github.com>
2024-12-02 13:16:21 -1000

    Fix test names

--- 2024/src/day22.rs
+++ 2024/src/day22.rs
@@ -3,10 +3,10 @@ use advent_of_code_common::read_data_from_file;
 
 fn main() -> Result<(), anyhow::Error> {
     let data = include_str!("../data/day22.txt");
-    let result = puzzle1(data).unwrap();
+    let result = crate::puzzle1(data).unwrap();
     println!("Day 22 Puzzle 1: {}", result);
 
-    let result = puzzle2(data).unwrap();
+    let result = crate::puzzle2(data).unwrap();
     println!("Day 22 Puzzle 2: {}", result);
     Ok(())
 }
@@ -21,19 +21,19 @@ fn puzzle2(_input: &str) -> Result<u32, anyhow::Error> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{puzzle1, puzzle2};
+    
 
     #[test]
-    fn day1() {
+    fn puzzle1() {
         let data = include_str!("../data/d22p1-test.txt");
-        let result = puzzle1(data);
+        let result = crate::puzzle1(data);
         assert_eq!(result.unwrap(), 142);
     }
 
     #[test]
-    fn day2() {
+    fn puzzle2() {
         let data = include_str!("../data/d22p2-test.txt");
-        let result = puzzle2(data);
+        let result = crate::puzzle2(data);
         assert_eq!(result.unwrap(), 281);
     }
 }
